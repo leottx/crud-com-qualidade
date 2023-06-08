4a26df58bfd9690341af72865f4cb25c096e0a2a
leottx <contact.leotx@gmail.com>
2023-06-08 11:40:20 -0300

    modulo-01: aula 01

---
+++ 01-modulo/nomenclatura-de-variaveis.js
@@ -0,0 +1,11 @@
+var n1 = 2;
+var n2 = 3;
+
+console.log(n1 + n2);
+
+// Como devs, o que mais fazemos é LER código!
+// Contexto é extremamente importante!
+const userFirstInputNumber = 10; // Especificando o tipo de dado no nome da variável induz a compreensão do resultado que deve ser esperado, no caso, um number.
+const userSecondInputNumber = 10;
+
+console.log(userFirstInputNumber + userSecondInputNumber);
