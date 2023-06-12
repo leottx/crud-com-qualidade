ec4ff390c6c7fb76b88a49352bbaf1b2f75b2f1f
leottx <contact.leotx@gmail.com>
2023-06-12 13:43:43 -0300

    modulo-01: aula 02 input, processamento e output

---
+++ 01-modulo/aula-02-input-processamento-output.js
@@ -0,0 +1,20 @@
+/* Tudo em tecnologia se resume a entrada -> processamento -> saída
+  - Front End (Web):
+    - Quem da a entrada?
+      - Usuário
+    - Quem processa?
+      - Navegador
+        - Desenvolvedor (JS ou alguma linguagem que compile para JS)
+    - Como é a saída?
+      - Elemento aparecendo na tela do usuário
+      - console.log()
+      - HTML + CSS
+  - Back End:
+    - Quem da a entrada?
+      - Usuário
+      - JSON
+    - Como é a saída?
+      - JSON
+      - Texto
+      - HTML
+*/
