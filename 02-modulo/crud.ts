952596ad44bb1d879f3be7d58c9a01b841645bb8
leotx <tteixeira.leonardo@gmail.com>
2023-06-28 10:58:31 -0300

    modulo-02: aula 03

--- 02-modulo/crud.ts
+++ 02-modulo/crud.ts
@@ -1,49 +1,66 @@
 // const fs = require('fs'); Padrão do common JS
 import fs from "fs"; // ES6
-const DB_FILE_PATH = './db';
+import { v4 as uuid } from "uuid";
+
+const DB_FILE_PATH = "./db";
 
 interface Todo {
-  date: string,
-  content: string,
-  done: boolean
+  id: string;
+  date: string;
+  content: string;
+  done: boolean;
 }
 
-function create(content: string) {
+function create(content: string): Todo {
   const todo: Todo = {
+    id: uuid(),
     date: new Date().toISOString(),
     content: content,
-    done: false
-  }
+    done: false,
+  };
 
-  const todos:Todo[] = [
-    ...read(),
-    todo,
-  ]
+  const todos: Todo[] = [...read(), todo];
 
   // Salva o valor contido no parâmetro content em um arquivo. Esse arquivo simula um banco de dados.
-  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({todos}, null, 2));
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({ todos }, null, 2));
+
+  return todo;
 }
 
-function read() {
-  const dbString = fs.readFileSync(DB_FILE_PATH, 'utf-8');
-  const db = JSON.parse(dbString || '{}');
-  
+function read(): Todo[] {
+  const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+  const db = JSON.parse(dbString || "{}");
+
   // Fail fast validation
   if (!db.todos) {
     return [];
   }
-  
+
   return db.todos;
 }
 
+function update(id: string, partialTodo: Partial<Todo>) {
+  const todos = read();
+
+  const todo = todos.find((todo) => todo.id === id);
+
+  if (!todo) return;
+
+  Object.assign(todo, partialTodo);
+
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({ todos }, null, 2));
+}
+
 function clearDB() {
-  fs.writeFileSync(DB_FILE_PATH, '');
+  fs.writeFileSync(DB_FILE_PATH, "");
 }
 
 clearDB();
 
-create('First file');
-create('Second file');
-create('Third file');
+create("First file");
+create("Second file");
 
-console.log(read());
+const thirdTodo = create("Third file");
+
+// Todo função de update de um CRUD recebe QUEM será atualizado e O QUE será o novo valor.
+update(thirdTodo.id, { content: "New for the third todo" });
